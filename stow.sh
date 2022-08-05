3fe3876b92f29e176704718683dd1c7c9af286f3
klar klarikon <zan.sprogar@gmail.com>
2022-08-05 14:28:26 +0000

    Moved into root dir, added bashrc check

---
+++ stow.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -f "~/.bashrc" ]; then
+    echo -e ".bashrc already exists. Do you want to replace it?"
+    read -p "[y/N]: " bashrc_choice
+    if [ "$bashrc_choice" == "y" ]; then
+        mv ~/.bashrc ~/.bashrc.old
+        stow bash
+    fi
+fi
+
+stow boomer
+stow bspwm
+stow gtk
+stow kitty
+stow mpd
+stow mpv
+stow nvim
+stow picom
+stow polybar
+stow rofi
+stow sxhkd
+stow when
+stow zathura

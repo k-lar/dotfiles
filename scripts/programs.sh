b43d64a2ca4c323c817291692714b10517eb0c09
k_lar <zan.sprogar@gmail.com>
2022-09-06 00:57:28 +0200

    Added pamixer for usage with my volume script

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -10,7 +10,7 @@
 # ===============================================================================
 
 CORE_PACKAGES=("neovim" "mpv" "feh" "zathura" "zathura-pdf-poppler" "xcolor" "thunar"
-"thunar-volman" "gvfs" "gvfs-mtp" "kitty" "when" "git" "rofi" "nomacs" "xclip"
+"thunar-volman" "gvfs" "gvfs-mtp" "kitty" "when" "git" "rofi" "nomacs" "xclip" "pamixer"
 "unrar" "zip" "unzip" "ncdu" "keepass" "file-roller" "exa" "ripgrep" "fzf" "btop"
 "pavucontrol" "flameshot" "bat" "stow" "ttf-ibm-plex" "emacs-nativecomp" "network-manager-applet")
 

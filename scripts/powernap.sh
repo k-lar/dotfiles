e81f7e343ca06b702599819f96e9d261ef2d4cbe
k_lar <zan.sprogar@gmail.com>
2022-10-03 01:23:54 +0200

    Shows time of initialization and notification warning

--- scripts/powernap.sh
+++ scripts/powernap.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+date=$(date +"%T")
 
-sleep $1 && poweroff
+printf "powernap started at: $date\n"
+sleep "$1"
+notify-send --app-name=powernap -w --urgency=critical "PC will shut down in 5m"
+echo "PC will shut down in 5m"
+sleep 5m
+poweroff

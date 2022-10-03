c2bf9f52337bc972bba1dd9d05231609a32bb59b
k_lar <zan.sprogar@gmail.com>
2022-10-03 08:08:36 +0200

    Fixed not running at all until notification was dismissed

--- scripts/powernap.sh
+++ scripts/powernap.sh
@@ -3,7 +3,7 @@ date=$(date +"%T")
 
 printf "powernap started at: $date\n"
 sleep "$1"
-notify-send --app-name=powernap -w --urgency=critical "PC will shut down in 5m"
+notify-send --app-name=powernap --expire-time=300000 --urgency=critical "PC will shut down in 5m"
 echo "PC will shut down in 5m"
 sleep 5m
 poweroff

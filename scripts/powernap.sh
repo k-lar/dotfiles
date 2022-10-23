32e936f30b23ad6f41d6c332ce713320fcb6769e
k_lar <zan.sprogar@gmail.com>
2022-10-23 18:32:44 +0200

    Now warn actually 5m before shutdown

--- scripts/powernap.sh
+++ scripts/powernap.sh
@@ -7,23 +7,48 @@ timeval=$(tr -d -c 'hms' <<< "$1")
 
 if [ "$timeval" = "h" ];then
     usrtime=$(echo "$usrtime*60" | bc | sed "s/\..*//")
+    if [ "$usrtime" > 5 ];then
+        time2sleep=$(echo "$usrtime-5" | bc | sed "s/\..*//")
+        dowarn=1
+    else
+        time2sleep=$(echo "$usrtime*60" | bc | sed "s/\..*//")
+        dowarn=0
+    fi
     datenext=$(date --date="$usrtime minutes" +"%T")
 fi
 
 if [ "$timeval" = "m" ];then
     usrtime=$(echo "$usrtime*60" | bc | sed "s/\..*//")
-    datenext=$(date --date="$usrtime seconds" +"%T")
+    if [ "$usrtime" > 300 ];then
+        time2sleep=$(echo "$usrtime-300" | bc | sed "s/\..*//")
+        dowarn=1
+    else
+        time2sleep=$(echo "$usrtime*60" | bc | sed "s/\..*//")
+        dowarn=0
+    fi
+    datenext=$(date --date="$usrtime  seconds" +"%T")
 fi
 
 if [ "$timeval" = "s" ];then
+    if [ "$usrtime" > 300 ];then
+        time2sleep=$(echo "$usrtime-300" | bc | sed "s/\..*//")
+        dowarn=1
+    else
+        time2sleep="$usrtime"
+        dowarn=0
+    fi
     datenext=$(date --date="$usrtime seconds" +"%T")
 fi
 
 printf "powernap started at: $date\n"
 printf "Your PC will poweroff at $datenext\n"
 
-sleep "$1"
-notify-send --app-name=powernap --expire-time=300000 --urgency=critical "PC will shut down in 5m"
-echo "PC will shut down in 5m"
-sleep 5m
+sleep "$time2sleep"
+
+if [ "$dowarn" = 1 ];then
+    notify-send --app-name=powernap --expire-time=300000 --urgency=critical "PC will shut down in 5m"
+    echo "PC will shut down in 5m"
+    sleep 5m
+fi
+
 poweroff

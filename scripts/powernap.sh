147b956200d2b9fd5bc6a7c81c3952f7b63a4806
k_lar <zan.sprogar@gmail.com>
2022-10-24 07:54:00 +0200

    More bugfixes

--- scripts/powernap.sh
+++ scripts/powernap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 date=$(date +"%T")
 
 usrtime=$(tr -d 'hms' <<< "$1")
@@ -9,9 +11,11 @@ if [ "$timeval" = "h" ];then
     usrtime=$(echo "$usrtime*60" | bc | sed "s/\..*//")
     if [ "$usrtime" -gt 5 ];then
         time2sleep=$(echo "$usrtime-5" | bc | sed "s/\..*//")
+        time2sleep="${time2sleep}m"
         dowarn=1
     else
         time2sleep="$usrtime"
+        time2sleep="${time2sleep}m"
         dowarn=0
     fi
     datenext=$(date --date="$time2sleep minutes" +"%T")
@@ -21,9 +25,11 @@ if [ "$timeval" = "m" ];then
     usrtime=$(echo "$usrtime*60" | bc | sed "s/\..*//")
     if [ "$usrtime" -gt 300 ];then
         time2sleep=$(echo "$usrtime-300" | bc | sed "s/\..*//")
+        time2sleep="${time2sleep}s"
         dowarn=1
     else
         time2sleep="$usrtime"
+        time2sleep="${time2sleep}s"
         dowarn=0
     fi
     datenext=$(date --date="$time2sleep seconds" +"%T")
@@ -32,9 +38,11 @@ fi
 if [ "$timeval" = "s" ];then
     if [ "$usrtime" -gt 300 ];then
         time2sleep=$(echo "$usrtime-300" | bc | sed "s/\..*//")
+        time2sleep="${time2sleep}s"
         dowarn=1
     else
         time2sleep="$usrtime"
+        time2sleep="${time2sleep}s"
         dowarn=0
     fi
     datenext=$(date --date="$time2sleep seconds" +"%T")

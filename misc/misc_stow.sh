5f2a10087aa11707e2c12d0a0551577fefa24df5
k_lar <zan.sprogar@gmail.com>
2022-11-10 15:07:48 +0100

    Added script for stowing misc + sh2dash pacman hook

---
+++ misc/misc_stow.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+if [ ! -f /usr/bin/stow ]; then
+    stow="perl $HOME/.dotfiles/bin/stow"
+    echo "Using bundled stow"
+else
+    stow="/usr/bin/stow"
+    echo "Using locally installed stow"
+fi
+
+for dir in ~/.dotfiles/misc/*/; do
+    [ -d "$dir" ] || continue
+    dir=$(basename "$dir")
+
+    case "$dir" in
+        touchpad) sh -c "sudo ${stow} -v -t /etc/X11/xorg.conf.d/ $dir";;
+        binsh2dash) sh -c "sudo ${stow} -v -t /usr/share/libalpm/hooks/ $dir";;
+        *) echo "Directory not recognized. Exiting..." || exit 1;;
+    esac
+
+done

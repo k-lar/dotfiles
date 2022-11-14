6a01ab8e029d301278b38d40a80ae65d6ec1d260
k_lar <zan.sprogar@gmail.com>
2022-11-15 00:16:28 +0100

    Added icons to notifications

--- scripts/toggle_esc2caps.sh
+++ scripts/toggle_esc2caps.sh
@@ -5,9 +5,9 @@ TOGGLE="$HOME/.toggle_caps2esc"
 if [ ! -e "$TOGGLE" ]; then
     touch "$TOGGLE"
     setxkbmap si -option
-    notify-send -u low -i mouse --icon=/usr/share/icons/Adwaita/32x32/status/changes-allow-symbolic.symbolic.png "Escape is normal"
+    dunstify -u low -r 178201 --icon=/usr/share/icons/Adwaita/32x32/status/changes-allow-symbolic.symbolic.png "Escape is normal"
 else
     rm "$TOGGLE"
     setxkbmap si -option 'caps:swapescape' -option 'caps:ctrl_modifier'; xmodmap -e 'keycode 255 = Escape'; xcape -e '#66=Escape' &
-    notify-send -u low -i mouse --icon=/usr/share/icons/Adwaita/32x32/status/changes-prevent-symbolic.symbolic.png "CapsLock is escape"
+    dunstify -u low -r 178201 --icon=/usr/share/icons/Adwaita/32x32/status/changes-prevent-symbolic.symbolic.png "CapsLock is escape"
 fi

efc5da8eaaebb3d55fd12825751d26109aaff52d
k_lar <zan.sprogar@gmail.com>
2022-12-02 11:14:37 +0100

    Added caffeine-ng to toggle monitor sleep

---
+++ scripts/toggle_caffeine.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Dependency:  caffeine-ng from aur
+# https://github.com/caffeine-ng/caffeine-ng
+
+TOGGLE="$HOME/.toggle_caffeine"
+
+if [ ! -e "$TOGGLE" ]; then
+    touch "$TOGGLE"
+    caffeine start &
+    dunstify -u low -r 1782123401 --icon=/usr/share/icons/Adwaita/32x32/status/weather-clear-symbolic.symbolic.png "Caffeine enabled"
+else
+    rm "$TOGGLE"
+    caffeine kill &
+    dunstify -u low -r 1782123401 --icon=/usr/share/icons/Adwaita/32x32/status/weather-clear-night-symbolic.symbolic.png "Caffeine disabled"
+fi
+

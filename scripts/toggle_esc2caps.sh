720c029d677130110e8593db6e91c38cfbd84352
k_lar <zan.sprogar@gmail.com>
2023-01-29 22:54:47 +0100

    Moved toggle files to /dev/shm instead

--- scripts/toggle_esc2caps.sh
+++ scripts/toggle_esc2caps.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-TOGGLE="$HOME/.dotfiles/options/.toggle_caps2esc"
+TOGGLE="/dev/shm/.toggle_caps2esc"
 
 if [ ! -e "$TOGGLE" ]; then
     touch "$TOGGLE"
-    setxkbmap si -option
+    setxkbmap -option
+    setxkbmap -layout si,ru -variant ,phonetic -option 'grp:alt_space_toggle'
     dunstify -u low -r 178201 --icon=/usr/share/icons/Adwaita/32x32/status/changes-allow-symbolic.symbolic.png "Escape is normal"
 else
     rm "$TOGGLE"
-    setxkbmap si -option 'caps:swapescape' -option 'caps:ctrl_modifier'; xmodmap -e 'keycode 255 = Escape'; xcape -e '#66=Escape' &
+    setxkbmap -layout si,ru -variant ,phonetic -option 'caps:swapescape' -option 'caps:ctrl_modifier' -option 'grp:alt_space_toggle'; xmodmap -e 'keycode 255 = Escape'; xcape -e '#66=Escape'
     dunstify -u low -r 178201 --icon=/usr/share/icons/Adwaita/32x32/status/changes-prevent-symbolic.symbolic.png "CapsLock is escape"
 fi

dd983fbfd9535617558d4275586e1d86ee131e8d
k_lar <sprogarzan@gmail.com>
2023-06-26 01:50:10 +0200

    Lazy loaded some stuff and got rid of old packer config

--- nvim/.config/nvim/lua/lazy-config.lua
+++ nvim/.config/nvim/lua/lazy-config.lua
@@ -15,24 +15,21 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     'ibhagwan/fzf-lua',
     'lewis6991/gitsigns.nvim',
-    'numToStr/Comment.nvim',
     'nvim-lualine/lualine.nvim',
-    'ggandor/leap.nvim',
     'https://gitlab.com/k_lar/dark-pale-gruvbox-lualine.nvim',
     'https://gitlab.com/k_lar/gruvbox-pale',
-    'dkarter/bullets.vim',
-    'dhruvasagar/vim-table-mode',
     'echasnovski/mini.indentscope',
     'folke/which-key.nvim',
     'echasnovski/mini.move',
-    'nvim-treesitter/playground',
+    'numToStr/Comment.nvim',
+    'ggandor/leap.nvim',
+    {'dhruvasagar/vim-table-mode', lazy = true},
+    {'dkarter/bullets.vim', lazy = true},
+    {'nvim-treesitter/playground', lazy = true},
 
     {
         -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-        },
         build = ':TSUpdate',
     }
 }, {})

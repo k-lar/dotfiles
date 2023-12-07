419b86dd8e6ac7ad501651c054783da4ed088d98
k_lar <sprogarzan@gmail.com>
2023-12-07 02:39:53 +0100

    Added LSP and autocompletion plugins again, also trying bamboo colorscheme

--- nvim/.config/nvim/lua/lazy-config.lua
+++ nvim/.config/nvim/lua/lazy-config.lua
@@ -16,8 +16,6 @@ require("lazy").setup({
     "ibhagwan/fzf-lua",
     "lewis6991/gitsigns.nvim",
     "nvim-lualine/lualine.nvim",
-    "https://gitlab.com/k_lar/dark-pale-gruvbox-lualine.nvim",
-    "https://gitlab.com/k_lar/gruvbox-pale",
     "folke/which-key.nvim",
     "stevearc/conform.nvim",
     "echasnovski/mini.move",
@@ -29,6 +27,30 @@ require("lazy").setup({
     { "dhruvasagar/vim-table-mode", lazy = true },
     { "dhruvasagar/vim-table-mode", ft = "plaintext", "markdown" },
     { "nvim-treesitter/playground", lazy = true },
+    {
+        "ribru17/bamboo.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("bamboo").setup({
+                style = "multiplex",
+                code_style = {
+                    comments = "none",
+                    conditionals = "none",
+                    keywords = "none",
+                    functions = "bold",
+                    namespaces = "none",
+                    parameters = "none",
+                    strings = "none",
+                    variables = "none",
+                },
+                highlights = {
+                    ["@comment"] = { fg = "$grey" },
+                },
+            })
+            require("bamboo").load()
+        end,
+    },
     {
         "folke/flash.nvim",
         event = "VeryLazy",
@@ -56,5 +78,33 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         lazy = true,
+    },
+    { -- LSP Configuration & Plugins
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            -- Automatically install LSPs to stdpath for neovim
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+
+            -- Useful status updates for LSP
+            { "j-hui/fidget.nvim", opts = {} },
+
+            -- Additional lua configuration, makes nvim stuff amazing!
+            "folke/neodev.nvim",
+        },
+    },
+    { -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+
+            -- Adds LSP completion capabilities
+            "hrsh7th/cmp-nvim-lsp",
+
+            -- Adds a number of user-friendly snippets
+            "rafamadriz/friendly-snippets",
+        },
     },
 }, {})

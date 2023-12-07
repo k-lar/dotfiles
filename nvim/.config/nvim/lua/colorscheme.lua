82db9e65f9c25775c0e436765be691d8f8f6f2bc
k_lar <sprogarzan@gmail.com>
2023-12-07 02:37:37 +0100

    Experimenting with bamboo colorscheme

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,20 +1,12 @@
-require("utils")
+-- require("utils")
 
 -- Colors
-o.background = "dark"
 o.termguicolors = true
-
-g.gruvbox_pale_style = "dark-pale"
-g.gruvbox_italic_comments = false
-g.gruvbox_italic_keywords = false
-g.gruvbox_bold_functions = true
-
-vim.cmd("colorscheme gruvbox-pale")
 
 -- Lualine setup
 require("lualine").setup({
     options = {
-        theme = "gruvbox-dark-pale",
+        theme = "bamboo",
         section_separators = "",
         component_separators = "",
     },

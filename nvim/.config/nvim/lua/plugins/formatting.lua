1960aee50454f3c081309a8fbcd8599a49b0a748
k_lar <zan.sprogar@gmail.com>
2024-07-09 01:59:17 +0200

    Added autoformat toggle commands and keybinds

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,9 +1,42 @@
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.autoformat = false
+    else
+        vim.g.autoformat = false
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.autoformat = true
+    vim.g.autoformat = true
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
+
+vim.keymap.set("n", "<leader>Fe", "<Cmd>FormatEnable<CR>", { desc = "Re-enable autoformat-on-save" })
+vim.keymap.set("n", "<leader>Fd", "<Cmd>FormatDisable<CR>", { desc = "Disable autoformat-on-save" })
+
+vim.keymap.set("n", "<leader>Ft", function()
+    vim.b.autoformat = not vim.b.autoformat
+    vim.notify("Autoformat on buffer: " .. tostring(vim.b.autoformat), vim.log.levels.INFO)
+end, { desc = "Toggle autoformat in this buffer" })
+
+vim.keymap.set("n", "<leader>FT", function()
+    vim.g.autoformat = not vim.g.autoformat
+    vim.notify("Autoformat globaly: " .. tostring(vim.g.autoformat), vim.log.levels.INFO)
+end, { desc = "Toggle autoformat in all buffers" })
+
 return {
     { "tpope/vim-sleuth" },
     { "vidocqh/auto-indent.nvim", opts = {} },
     {
         "stevearc/conform.nvim",
         opts = {
+            notify_on_error = false,
             formatters_by_ft = {
                 lua = { "stylua" },
                 -- Conform will run multiple formatters sequentially
@@ -12,13 +45,19 @@ return {
                 -- Use a sub-list to run only the first available formatter
                 javascript = { { "prettierd", "prettier" } },
             },
+            format_on_save = function(bufnr)
+                local disable_filetypes = { c = true, cpp = true }
+                -- Disable with a global or buffer-local variable
+                if vim.g.autoformat == false or vim.b[bufnr].autoformat == false then
+                    return
+                end
+
+                return {
+                    timeout_ms = 500,
+                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                }
+            end,
         },
-        init = function() -- Autoformat on save
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*",
-                callback = function(args) require("conform").format({ bufnr = args.buf }) end,
-            })
-        end,
     },
     {
         "windwp/nvim-autopairs",
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -2,6 +2,7 @@ return {
     {
         "folke/which-key.nvim",
         opts = {},
+        event = "VeryLazy",
         config = function()
             require("which-key").register({
                 ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
@@ -10,8 +11,10 @@ return {
                 ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
                 ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
                 ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+                ["<leader>S"] = { name = "[S]pell", _ = "which_key_ignore" },
                 ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
                 ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+                ["<leader>F"] = { name = "[F]ormatting", _ = "which_key_ignore" },
             })
         end,
     },

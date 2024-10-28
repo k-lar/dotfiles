01d83b790be38591660c0694eba4e1b8b3b9308f
k_lar <zan.sprogar@gmail.com>
2024-10-28 13:29:02 +0100

    Updated which-key config

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,10 +1,44 @@
 return {
     {
         "folke/which-key.nvim",
-        opts = {},
-        event = "VeryLazy",
-        config = function()
-            require("which-key").add({
+        opts = {
+            icons = {
+                -- set icon mappings to true if you have a Nerd Font
+                mappings = vim.g.have_nerd_font,
+                -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+                -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+                keys = vim.g.have_nerd_font and {} or {
+                    Up = "<Up> ",
+                    Down = "<Down> ",
+                    Left = "<Left> ",
+                    Right = "<Right> ",
+                    C = "<C-…> ",
+                    M = "<M-…> ",
+                    D = "<D-…> ",
+                    S = "<S-…> ",
+                    CR = "<CR> ",
+                    Esc = "<Esc> ",
+                    ScrollWheelDown = "<ScrollWheelDown> ",
+                    ScrollWheelUp = "<ScrollWheelUp> ",
+                    NL = "<NL> ",
+                    BS = "<BS> ",
+                    Space = "<Space> ",
+                    Tab = "<Tab> ",
+                    F1 = "<F1>",
+                    F2 = "<F2>",
+                    F3 = "<F3>",
+                    F4 = "<F4>",
+                    F5 = "<F5>",
+                    F6 = "<F6>",
+                    F7 = "<F7>",
+                    F8 = "<F8>",
+                    F9 = "<F9>",
+                    F10 = "<F10>",
+                    F11 = "<F11>",
+                    F12 = "<F12>",
+                },
+            },
+            spec = {
                 { "<leader>F", group = "[F]ormatting" },
                 { "<leader>F_", hidden = true },
                 { "<leader>S", group = "[S]pell" },
@@ -15,8 +49,6 @@ return {
                 { "<leader>d_", hidden = true },
                 { "<leader>g", group = "[G]it" },
                 { "<leader>g_", hidden = true },
-                { "<leader>h", group = "Git [H]unk" },
-                { "<leader>h_", hidden = true },
                 { "<leader>r", group = "[R]ename" },
                 { "<leader>r_", hidden = true },
                 { "<leader>s", group = "[S]earch" },
@@ -25,7 +57,8 @@ return {
                 { "<leader>t_", hidden = true },
                 { "<leader>w", group = "[W]orkspace" },
                 { "<leader>w_", hidden = true },
-            })
-        end,
+            },
+        },
+        event = "VimEnter",
     },
 }

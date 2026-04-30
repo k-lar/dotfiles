b360774657d9e7131f2748b1363eed33f0976bb6
k_lar <zan.sprogar@gmail.com>
2026-05-01 01:10:22 +0200

    Removed incremental selection keybinds as they are built-in now with vin and van

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -29,12 +29,4 @@ treesitter.setup({
     },
     highlight = { enable = true },
     indent = { enable = false },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "vv",
-            node_incremental = "vv",
-            node_decremental = "vV",
-        },
-    },
 })

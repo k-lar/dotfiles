16f8dc3a51c6f527331c14efb13eeb438af6621f
k_lar <zan.sprogar@gmail.com>
2026-06-06 18:33:48 +0200

    Added keybind to throw a window to another workspace but don't follow it

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -162,6 +162,7 @@ for i = 1, 10 do
     local key = i % 10 -- 10 maps to key 0
     hl.bind(mainMod .. " + "         .. key,     hl.dsp.focus({ workspace = i }))
     hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + CTRL + "  .. key,     hl.dsp.window.move({ workspace = i, follow = false }))
 end
 
 -- Alt + Tab to switch windows
